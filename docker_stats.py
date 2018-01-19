6b47c6ff5db9f422aad2ad5177664fd963a85a6a
Joy Rahman <joyrahman@gmail.com>
2018-01-19 11:29:04 -0600

    new update

--- docker_stats.py
+++ docker_stats.py
@@ -11,10 +11,11 @@ def poll_data():
     ts = datetime.datetime.utcnow()
     output = "{},".format(ts)
 
-    for line in p.stdout.readlines():
+    for line in p.stdout.readlines()[1:]:
         data = re.split('\s+',line)
         key = data[0]
-        cpu = data[1]
+        cpu = data[2]
+        print(data)
         if key in record:
            output += "{},{},".format(record[key],cpu)
     return output
@@ -34,6 +35,14 @@ for line in p.stdout.readlines():
     key  = data2[0]
     value = data2[1].split('/')[1]
     record[key] = value
+
+p = subprocess.Popen('docker ps | grep "mongo"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+for line in p.stdout.readlines():
+    data2 = re.split('\s+',line)
+    key  = data2[0]
+    value = data2[1]
+    record[key] = value
+print(record)
 
 
 ### 2nd phase
